b9f28a810c4e8fe6f79fca17a526a527c616ab64
YOUNGJIN JOO <neoelec@gmail.com>
2025-02-17 03:08:44 +0900

    chap05/03_quicksrt
    Signed-off-by: YOUNGJIN JOO <neoelec@gmail.com>

---
+++ chap05/03_quicksrt/test_quicksrt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+#include <rcn/quicksrt.h>
+
+static int int_compare(const void *a, const void *b)
+{
+    return *(const int *)a - *(const int *)b;
+}
+
+int main(void)
+{
+    int data[] = { 6, 4, 2, 3, 1, 5 };
+    size_t i;
+
+    quicksrt(data, NR_ELEM(data), sizeof(data[0]), int_compare);
+
+    for (i = 0; i < NR_ELEM(data); i++)
+        printf("%d ", data[i]);
+
+    printf("\n");
+
+    return 0;
+}
