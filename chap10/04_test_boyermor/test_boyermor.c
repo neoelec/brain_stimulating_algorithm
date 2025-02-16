3da27766a5c4069f7b447116b6feb5aabb2d20a2
YOUNGJIN JOO <neoelec@gmail.com>
2025-02-17 03:08:44 +0900

    chap10/04_test_boyermor
    Signed-off-by: YOUNGJIN JOO <neoelec@gmail.com>

---
+++ chap10/04_test_boyermor/test_boyermor.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <string.h>
+
+#include <rcn/boyermor.h>
+
+#define MAX_BUFFER 512
+
+int main(int argc, char *argv[])
+{
+    char *file_name;
+    FILE *fp;
+    char text[MAX_BUFFER];
+    char *pattern;
+    size_t pattern_len;
+    size_t line = 0;
+
+    if (argc < 3) {
+        printf("Usage: %s <file_name> <pattern>\n", argv[0]);
+        return 1;
+    }
+
+    file_name = argv[1];
+    pattern = argv[2];
+
+    pattern_len = strlen(pattern);
+
+    if ((fp = fopen(file_name, "r")) == NULL) {
+        printf("Cannot open file: %s\n", file_name);
+        return 1;
+    }
+
+    while (fgets(text, MAX_BUFFER, fp) != NULL) {
+        ssize_t pos = boyermor(text, strlen(text), 0, pattern, pattern_len);
+
+        line++;
+
+        if (pos >= 0)
+            printf("line: %zu, column: %zd : %s", line, pos + 1, text);
+    }
+
+    fclose(fp);
+
+    return 0;
+}
