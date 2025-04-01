e3c31fa4b53e7d07ede483b6ab73cd13c66b4201
YOUNGJIN JOO <neoelec@gmail.com>
2025-04-02 01:43:51 +0900

    chap01: 03_test_cdll -> 03_lldq
    Signed-off-by: YOUNGJIN JOO <neoelec@gmail.com>

---
+++ chap01/03_lldq/test_lldq.c
@@ -0,0 +1,101 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <rcn/lldq.h>
+
+struct lldq_entry {
+    struct dbll_node node;
+    int data;
+};
+
+static struct lldq_entry *lldq_create_entry(int data)
+{
+    struct lldq_entry *entry;
+
+    entry = malloc(sizeof(*entry));
+    if (entry == NULL) {
+        fprintf(stderr, "Failed to allocate memory for list entry\n");
+        exit(EXIT_FAILURE);
+    }
+
+    entry->data = data;
+
+    return entry;
+}
+
+static void lldq_print_entry(struct dbll_node *node, void *private)
+{
+    ssize_t *idx = private;
+    struct lldq_entry *entry = container_of(node, struct lldq_entry, node);
+
+    printf("list[%zd] : %d\n", (*idx)++, entry->data);
+}
+
+int main(void)
+{
+    ssize_t i;
+    struct lldq __list, *list = &__list;
+    struct dbll_node *node;
+    struct lldq_entry *entry;
+    ssize_t count;
+
+    lldq_initialize(list);
+
+    /* 노드 5개 추가 */
+    for (i = 0; i < 5; i++) {
+        entry = lldq_create_entry(i);
+        lldq_push_rear(list, &entry->node);
+    }
+
+    /* 뎈 출력 */
+    i = 0;
+    lldq_forward(list, lldq_print_entry, &i);
+
+    /* 뎈의 세 번째 노드 뒤에 새 노드 삽입 */
+    printf("\nInserting 3000 After [2]...\n\n");
+    entry = lldq_create_entry(3000);
+    lldq_insert(list, 3, &entry->node);
+
+    /* 뎈 출력 */
+    i = 0;
+    lldq_forward(list, lldq_print_entry, &i);
+
+    printf("\nRemoving Node at 2...\n");
+    lldq_remove(list, 2, &node);
+    free(container_of(node, struct lldq_entry, node));
+
+    /* 뎈 출력 */
+    /* (노드 수의 2배만큼 루프를 돌며 환형임을 확인한다.) */
+    count = lldq_count(list);
+    for (i = 0; i < count * 2; i++) {
+        lldq_get(list, i, &node);
+        entry = container_of(node, struct lldq_entry, node);
+        printf("list[%zd] : %d\n", i, entry->data);
+    }
+
+    /* 뎈 뒤집기 */
+    printf("\nReversing List...\n");
+    lldq_reverse(list);
+
+    /* 뎈 출력 */
+    i = 0;
+    lldq_forward(list, lldq_print_entry, &i);
+
+    /* 뎈 출력 */
+    printf("\nNegative Index...\n");
+    count = lldq_count(list);
+    for (i = 1; i <= count; i++) {
+        lldq_get(list, -i, &node);
+        entry = container_of(node, struct lldq_entry, node);
+        printf("list[%zd] : %d\n", -i, entry->data);
+    }
+
+    /* 모든 노드를 메모리에서 제거 */
+    printf("\nDestroyihng List...\n");
+    while (!lldq_is_empty(list)) {
+        lldq_pop_rear(list, &node);
+        free(container_of(node, struct lldq_entry, node));
+    }
+
+    return 0;
+}
